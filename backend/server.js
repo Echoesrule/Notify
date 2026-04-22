90c8bba7592cfa6a289ecf20ebc787c26bd085ab
Echoesrule <kipronoemmanuel911@gmail.com>
2026-04-22 09:11:25 +0300

    Fixed routes not working

--- backend/server.js
+++ backend/server.js
@@ -94,6 +94,7 @@ app.use(cors({
 // =====================
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.enable('trust proxy');
 app.use('/uploads', express.static(uploadsDir));
 
 app.use((req, res, next) => {
